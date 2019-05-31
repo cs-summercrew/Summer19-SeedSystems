7b9f4c9558812e73affed06154cfda50269f1c6c
Dylan McGarvey <summer19@Summers-iMac.local>
2019-05-31 13:26:44 -0700

    Update selTest.py

--- SeleniumScraper/selTest.py
+++ SeleniumScraper/selTest.py
@@ -1,15 +1,16 @@
 
 # NOTE: Installation Instructions for Firefox
-# Use "pip install selenium" to install selenium for python
-# Go to https://addons.mozilla.org/en-US/firefox/addon/selenium-ide/ to install the Selenium add-on for Firefox
-# Go to https://github.com/mozilla/geckodriver/releases to install geckodriver
-# Make sure to choose the geckodriver file that corresponds to your OS
-# More helpful documentation info at https://selenium-python.readthedocs.io/getting-started.html
+#       Use "pip install selenium" to install selenium for python
+#       Go to https://addons.mozilla.org/en-US/firefox/addon/selenium-ide/ to install the Selenium add-on for Firefox
+#       Go to https://github.com/mozilla/geckodriver/releases to install geckodriver
+#       Make sure to choose the geckodriver file that corresponds to your OS
+#       Make sure that you update the path to geckodriver in main()
+#       More helpful documentation info at https://selenium-python.readthedocs.io/getting-started.html
 
 import os
 import time
-#NOTE: We use sleep from the time library because the code often runs faster than pages can load.
-#      We also added a few extra so that you can watch what is happening.
+# NOTE: We use sleep from the time library because the code often runs faster than pages can load.
+#       We also added a few extra so that you can watch what is happening.
 try:
     from selenium import webdriver
     from selenium.webdriver.common.keys import Keys
@@ -71,7 +72,7 @@ def websearch(driver):
     #     print("Your Internet must suck! The webpage took too long to load.")
     
     time.sleep(2)
-    search_form = driver.find_element_by_id('search_form_input')
+    search_form = driver.find_element_by_id("search_form_input")
     search_form.clear()
     search_form.send_keys("esoteric programming languages churro")
     search_form.submit()
@@ -99,7 +100,7 @@ def buttonclick(driver, button_element):
 def closepopup(driver):
     "Closes the duckduckgo add-on popup for FIREFOX only"
     try: 
-        # NOTE: You can find the xpath of an element by right-clikcing it in the browser
+        # NOTE: You can find the xpath of an element by right-clicking it in the browser
         #       and clicking inspect element. Right click on the respective html code, and
         #       you should see an option to copy the element's XPATH
         close_button = driver.find_element(By.XPATH, "/html/body/div/div[5]/a/span")
@@ -147,6 +148,7 @@ def main():
     
     driver = createdriver('https://duckduckgo.com',
     '/Users/summer19/Documents/GitHub/Summer19-SeedSystems/SeleniumScraper/geckodriver')
+    # NOTE: You will need to change the above path to wherever you have installed geckodriver
     closepopup(driver)
     websearch(driver)
     useform(driver)
