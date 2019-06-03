eaa3ac6d3ee55aaf00762f8d706f3bd748774034
Justin Gadalla <jgadalla@g.hmc.edu>
2019-06-03 11:41:21 -0700

    First commit of Flask App example

---
+++ ExampleApp/app/routes.py
@@ -0,0 +1,74 @@
+try:
+    from flask import render_template, redirect, url_for, request, send_from_directory, flash
+except:
+    print("Make sure to pip install Flask twilio")
+from app import app
+import os
+from werkzeug import secure_filename
+from app import piglatin
+try:
+    from PIL import Image
+    import PIL.ImageOps
+except:
+    print("Make sure to pip install Pillow")
+
+# Home page, renders the index.html template
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+# Pig latin page, when we click translate, moves to text result page
+@app.route('/text',methods=['GET','POST'])
+def text():
+    if request.method == 'POST':
+        old_text = request.form['text']
+        new_text = piglatin.pig_translate(old_text)
+        return render_template('textResults.html', old_text=old_text, new_text=new_text)
+    return render_template('text.html', title='Home')
+
+# Used for uploading pictures
+@app.route('/<filename>')
+def get_file(filename):
+    return send_from_directory('static',filename)
+
+# Image uploading page, 
+@app.route('/image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        # if the image is valid, do the following
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # Create a path to the image in the upload folder, save the upload
+            # file to this path
+            save_old=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(save_old)
+            # Take the image, make a new one that is inverted
+            img = Image.open(save_old)
+            rbg_img = img.convert('RGB')
+            inverted_image = PIL.ImageOps.invert(rbg_img)
+            save_new=(os.path.join(app.config['UPLOAD_FOLDER'], 'new_'+filename))
+            inverted_image.save(save_new)
+            # Render template with inverted picture
+            rt = render_template('imageResults.html', filename='new_'+filename)
+            return rt
+    return render_template('image.html')
+
+# allowed image types 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['ALLOWED_EXTENSIONS']=ALLOWED_EXTENSIONS
+
+# is file allowed to be uploaded?
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
---
+++ ExampleApp/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Set the upload folder as the static folder in app directory
+    UPLOAD_FOLDER = 'app/static'
