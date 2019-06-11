27a85761cddfcc55d3a7d7f9139e82697d5aa82f
Dylan McGarvey <summer19@Summers-iMac.local>
2019-06-10 17:02:31 -0700

    update

---
+++ OpenCVExamples/ImageProcessing/comparing-histograms.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import os.path
+import shutil
+
+def histCompare():
+    #https://docs.opencv.org/2.4.13.6/doc/tutorials/imgproc/histograms/histogram_comparison/histogram_comparison.html?highlight=histogram
+    # Do something showing off these functions
+    cv.compareHist(x,x,CV_COMP_CORREL)
+    # cv2.HISTCMP_CHISQR
+    # cv2.HISTCMP_INTERSECT
+    # cv2.HISTCMP_BHATTACHARYYA
+    # TODO: use the different metrics on a folder of images to compare to a single image
+    return
+
+def main():
+    # histCompare()
+
+if __name__ == "__main__":
+    main()
--- OpenCVExamples/ImageProcessing/other-histogram-functions.py
+++ OpenCVExamples/ImageProcessing/other-histogram-functions.py
@@ -71,7 +71,11 @@ def histEqual(file):
 def histCompare():
     #https://docs.opencv.org/2.4.13.6/doc/tutorials/imgproc/histograms/histogram_comparison/histogram_comparison.html?highlight=histogram
     # Do something showing off these functions
-    cv.compareHist(x,x,CV_COMP_CORREL) # Show off the different metrics
+    cv.compareHist(x,x,CV_COMP_CORREL)
+    # cv2.HISTCMP_CHISQR
+    # cv2.HISTCMP_INTERSECT
+    # cv2.HISTCMP_BHATTACHARYYA
+    # TODO: use the different metrics on a folder of images to compare to a single image
     return
 
 def histColorMatch(srcFile, tempFile):
