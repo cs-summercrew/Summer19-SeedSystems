9712d5e92829eff423e47e6dd916bca5f20684df
Dylan McGarvey <summer19@Summers-iMac.local>
2019-06-17 17:06:38 -0700

    Good Progress

--- MLPipelines/Classification-Pipeline.py
+++ MLPipelines/Classification-Pipeline.py
@@ -20,9 +20,9 @@ from sklearn.svm import SVC
 answers = [9,9,5,5,6,5,0,9,8,9,8,4,0,1,2,3,4,5,6,7]
 
 
-def accuracyBoxPlot(results, names):
+def boxPlot(results, names):
     fig = plt.figure()
-    fig.suptitle('Algorithm Accuracy Comparison')
+    fig.suptitle('Algorithm Comparison')
     ax = fig.add_subplot(111)
     plt.boxplot(results)
     ax.set_xticklabels(names)
@@ -41,17 +41,27 @@ def crossValidation(X_known, y_known):
     models.append( ("Support Vector Machine ",SVC(gamma="scale")) )
 
     # evaluate each model in turn
-    results = []
+    AccResults = []
+    PrcResults = []
     names = []
-    scoring = 'accuracy'
-    print("\nAlgorithm : Mean Accuracy (95% Confidence Interval), \n")
+    # NOTE: See different scoring params: https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
+    scoring = ['accuracy','f1_weighted', 'precision_weighted']
+    # BUG: Check that I am using the right type of scoring params
+    #      Also check the confidence interval calculation
+    
+    print("\nAlgorithm : Accuracy, Weighted f1 score, Weighted Precision")
+    print("*** Results show means for each scoring metric, with 95% Confidence Intervals in parenthesis\n")    
     for name, model in models:
         kfold = model_selection.KFold(n_splits=10, random_state=None)
-        cv_scores = model_selection.cross_val_score(model, X_known, y_known, cv=kfold, scoring=scoring)
-        results.append(cv_scores)
+        cv_scores = model_selection.cross_validate(model, X_known, y_known, cv=kfold, scoring=scoring)
+        AccResults.append(cv_scores["test_accuracy"])
+        PrcResults.append(cv_scores["test_precision_weighted"])
         names.append(name)
-        print( "%s: %0.3f (+/- %0.3f)," % (name, cv_scores.mean(), cv_scores.std() * 2),"hi" )
-    # accuracyBoxPlot(results, names)
+        print( "%s: %0.3f (+/- %0.3f)," % (name, cv_scores["test_accuracy"].mean(), cv_scores["test_accuracy"].std() * 2),
+        "%.3f (+/- %0.3f)," % (cv_scores["test_f1_weighted"].mean(), cv_scores["test_f1_weighted"].std() * 2),
+        "%.3f (+/- %0.3f)" % (cv_scores["test_precision_weighted"].mean(), cv_scores["test_precision_weighted"].std() * 2) )
+    boxPlot(AccResults, names)
+    boxPlot(PrcResults, names)
     return
 
 def main():
